102ec8470063e2c7393a45faa9581013775720e2
jkrula <jkrula@yahoo.com>
2017-10-22 19:41:46 +0300

    add google street view image for a background

--- js/script.js
+++ js/script.js
@@ -6,15 +6,25 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-
+    
+    var $street = $('#street').val();
+    var $city = $('#city').val();
+    var fulladress = $street +", "+$city;
+    
+    $greeting.text("So you want to live at "+fulladress+"?");
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-
-    // YOUR CODE GOES HERE!
-
+    // load google streetview
+    var googleUrl= "https://maps.googleapis.com/maps/api/streetview?size=600x400&location="+fulladress;
+    
+    $body.append('<img class="bgimg" src ="'+googleUrl+'">');
+    
+    
+    
+    
+    
     return false;
 };
 
